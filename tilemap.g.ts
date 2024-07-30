d366bf9ac8b61ad8a821ffbe2720097d2223ada5
r4forth <r4forth@gmail.com>
2024-07-30 20:14:59 +0800

    新增了一條道路並加上一些花朵

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,6 +2,12 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -25,7 +31,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "層級2":
-            case "層級2":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010101010101010101010101010101010101020202020202020101010101010101010201010101010201010101010101010202010101020202010101010101010102010101010201010101010101010101020101010102020202020202020201010202010101010101010101010102010101020101010101010101010101020101010202020202010101010202020201010101010102020101010102010101010101010202020101010202020101010101010102010101010102010101010101010101020101010101020101010101010101010202020202020201010101010101010101010101010101010101010101`, img`
+            case "層級2":return tiles.createTilemap(hex`1000100003010101030101010301010301010301010101010101010101010101010101010301020202020202020101030101030101010201010101010201010101010101010202010101020202010103010103010302010304030201010101010101010101020101040102020202020202020201030202030403010103010103010102010101020105060606070101010101020101010202020202030401030202020201010301010102020104010102010101010101010202020101040202020301030103010302010301060802030101010101010301020301030101020101030103010301030202020202020201030103010301010101010101010101010101010101`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . 2 2 2 2 2 2 2 . . . . . . . 
@@ -42,7 +48,7 @@ namespace myTiles {
 . . . 2 . . . . . 2 . . . . . . 
 . . . 2 2 2 2 2 2 2 . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.dungeon.floorLight0], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.castle.tileGrass1,sprites.dungeon.floorLight0,sprites.castle.tileGrass2,sprites.vehicle.roadVertical,sprites.vehicle.roadTurn3,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn2,sprites.vehicle.roadTurn4,myTiles.tile1,myTiles.tile2], TileScale.Sixteen);
         }
         return null;
     })
@@ -50,6 +56,12 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "我的貼圖":
+            case "tile1":return tile1;
+            case "我的貼圖0":
+            case "tile2":return tile2;
+            case "我的貼圖1":
+            case "tile3":return tile3;
         }
         return null;
     })
